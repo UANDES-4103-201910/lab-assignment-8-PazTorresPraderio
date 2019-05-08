936bdb1d9768f316b9c5f654ff1bed73fbe07535
PazTorresPraderio <cptorres@miuandes.cl>
2019-05-08 11:57:14 -0300

    Few things

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,18 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+$(document).ready(function(){
+	$(".key").click(function(){
+		console.log($(this).text());
+		$("#textbox"());
+	});
+	$("#buttomHide").click(function(){
+		$(".key").hide();
+
+	});
+	$("#buttomShow").click(function(){
+		$(".key").show();
+
+	});
 });
+	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+
+// $("#printingKeys").click(function(){
